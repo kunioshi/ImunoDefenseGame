d40a27ff2f4e1e6fe96c6a0b2668975cc8428c5d
francisconin <francnf@gmail.com>
2013-05-24 18:11:08 -0300

    Separação entre locais de criação e exclusão de torres. Testes!

--- src/MouseHandler.java
+++ src/MouseHandler.java
@@ -5,35 +5,50 @@ import java.awt.event.MouseMotionListener;
 import java.io.IOException;
 
 import s3t.gameControl.system.GameSystem;
+import s3t.gameEntities.Entity;
+import s3t.gameEntities.Scenario;
 
 
 public class MouseHandler implements MouseListener, MouseMotionListener {
 	public static Point mousePos = new Point();
 	public static MouseHandler mouseHandler = new MouseHandler();
+	public SegueMouse spider;
+	public Cenario_Basico cenario;
 	
 	@Override
 	public void mouseClicked(MouseEvent e) {
 
 		int gridX = (int) (e.getX() / 32);
 		int gridY = (int) (e.getY() / 32);
-		boolean estaSeguindo = false;
+		int gridtesteY = 12;
 		
 		System.out.println(gridX + "|" + gridY);
 		
+		
+		
 		try {
-			SegueMouse spider = new SegueMouse(mousePos.x, mousePos.y, GameSystem.getScenarioCollection().getScenarioAtual());
-			if (spider != null){estaSeguindo = true;}
-			//Spider spider = new Spider(gridX*32, gridY*32, GameSystem.getScenarioCollection().getScenarioAtual());
-			GameSystem.getEntityCollection().addEntity(spider);
-			if (estaSeguindo = true){
+			
+			if (gridY >= gridtesteY)
+			{				
+				spider = new SegueMouse(mousePos.x, mousePos.y, GameSystem.getScenarioCollection().getScenarioAtual());
+				GameSystem.getEntityCollection().addEntity(spider);
+				//Spider aranha = new Spider(gridX*32, gridY*32, GameSystem.getScenarioCollection().getScenarioAtual());
+			}else{
 				PontosDeColisao ponto = new PontosDeColisao(){};
 				ponto.colisao.add(new Point(gridX, gridY));
+				
+				//Entity teste = new Entity(teste, gridX, gridY, cenario)
+				GameSystem.getEntityCollection().removeEntity(spider);
 			}
+			
+			
+			
 			
 
 		} catch (IOException e1) {
 			System.out.println("Erro na imagem spider");
 		}
+		
 	}
 
 	@Override
