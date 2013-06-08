06b215bbad336693d976b47e7e20361a68d2f92f
Kunioshi <Kunioshi@Kunioshi-PC>
2013-06-08 00:56:01 -0300

    random commit

--- src/main/java/br/envyGames/imunoDefense/jogo/InimigoGripe.java
+++ src/main/java/br/envyGames/imunoDefense/jogo/InimigoGripe.java
@@ -5,10 +5,13 @@ import java.awt.Point;
 import br.envyGames.imunoDefense.motor.Cenario;
 
 import s3t.gameControl.system.GameSystem;
+import s3t.graphicsElements.ImageCollection;
 
 public class InimigoGripe extends Inimigo {
 	public InimigoGripe(String name, Point xy, Cenario cenario) {
 		super(name, xy, cenario);
+		
+		//ImageCollection imgCollection = ;
 		
 		GameSystem.setAIforEntity(this, new AIGripe());
 	}
