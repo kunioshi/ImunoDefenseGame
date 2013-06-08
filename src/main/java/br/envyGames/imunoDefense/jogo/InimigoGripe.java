f8a5ffa58c84a88c4bfa138d1e0cff11c1e5a5a4
Kunioshi <Kunioshi@Kunioshi-PC>
2013-06-07 23:53:37 -0300

    Arrumando o construtor do Gripe

--- src/main/java/br/envyGames/imunoDefense/jogo/InimigoGripe.java
+++ src/main/java/br/envyGames/imunoDefense/jogo/InimigoGripe.java
@@ -2,11 +2,13 @@ package br.envyGames.imunoDefense.jogo;
 
 import java.awt.Point;
 
+import br.envyGames.imunoDefense.motor.Cenario;
+
 import s3t.gameControl.system.GameSystem;
 
 public class InimigoGripe extends Inimigo {
-	public InimigoGripe(String name, Point xy) {
-		super(name, xy);
+	public InimigoGripe(String name, Point xy, Cenario cenario) {
+		super(name, xy, cenario);
 		
 		GameSystem.setAIforEntity(this, new AIGripe());
 	}
