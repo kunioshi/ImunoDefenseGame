b2f919835b21371c33e4ec6365a6e2ba57c8bf6f
Lucas <martino.mail@gmail.com>
2013-06-07 23:16:08 -0300

    Incluindo gerenciador de entidade no motor

--- src/main/java/br/envyGames/imunoDefense/motor/Entidade.java
+++ src/main/java/br/envyGames/imunoDefense/motor/Entidade.java
@@ -4,9 +4,8 @@ import s3t.gameEntities.Entity;
 
 public class Entidade extends Entity {
 
-	public Entidade(String name, double x, double y, Cenario cenario) {
-		super(name, x, y, cenario);
-		// TODO Auto-generated constructor stub
+	public Entidade(String nome, double x, double y, Cenario cenario) {
+		super(nome, x, y, cenario);
 	}
 
 }
