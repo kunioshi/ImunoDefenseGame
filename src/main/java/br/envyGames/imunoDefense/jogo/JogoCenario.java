cef6c99741148858aaf0440f192271397ded30e9
Lucas <martino.mail@gmail.com>
2013-06-10 18:42:54 -0300

    Incluindo senario do jogo

---
+++ src/main/java/br/envyGames/imunoDefense/jogo/JogoCenario.java
@@ -0,0 +1,21 @@
+package br.envyGames.imunoDefense.jogo;
+
+import java.awt.Color;
+
+import br.envyGames.imunoDefense.motor.Cenario;
+import br.envyGames.imunoDefense.motor.CenarioLayer;
+
+public class JogoCenario extends Cenario {
+
+	public JogoCenario(int largura, int altura) {
+		super("JogoCenario", "Jogo", largura, altura);
+		
+
+	    setBackgroundColor(Color.black);
+		
+		CenarioLayer background = CenarioLayer.criarSolidLayer("BackGround");
+		
+		adicionarLayer(background);
+	}
+		
+}
