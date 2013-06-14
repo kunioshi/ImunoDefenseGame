1ee58a18e167d826bf6abd07931c64187208fd02
Lucas <martino.mail@gmail.com>
2013-06-14 15:56:38 -0300

    Animacao coracao

--- src/main/java/br/envyGames/imunoDefense/jogo/Coracao.java
+++ src/main/java/br/envyGames/imunoDefense/jogo/Coracao.java
@@ -10,24 +10,36 @@ import br.envyGames.imunoDefense.motor.ResourceManager;
 public class Coracao extends FormaDeVida {
 
 	private static Imagem coracao = ResourceManager.getImagem("/imagens/entidades/CoracaoVivo.png");
+	private static Imagem coracao50porcento = ResourceManager.getImagem("/imagens/entidades/Coracao50porcento.png");
+	private static Imagem coracao20porcento = ResourceManager.getImagem("/imagens/entidades/Coracao20porcento.png");
+	private ImagemColecao imagemColecao;
+	private final int TOTAL_VIDA = 20;
 	
 	public Coracao(Point xy, Cenario cenario) {
 		super("Coracao", xy, cenario);
 		
-		vida = 20;
+		vida = TOTAL_VIDA;
 		
-		ImagemColecao imagemColecao = new ImagemColecao();
+		imagemColecao = new ImagemColecao();
 		imagemColecao.add("vivo", coracao);
+		imagemColecao.add("50porcento", coracao50porcento);
+		imagemColecao.add("20porcento", coracao20porcento);
 		imagemColecao.definirImagemPadrao("vivo");
         
         definirImagemColecao(imagemColecao);
         setDoNotStop(true);
 	}
-
+	
 	@Override
-	public void morrer() {
-//		motor.loadCenario("GameOverCenario");
-//		motor.getEntidadeGerenciador().clear();
+	public void receberDano(int dano) {
+		super.receberDano(dano);
 		
+		if (getVida() <= TOTAL_VIDA * 0.5) {
+			
+			if (getVida() <= TOTAL_VIDA * 0.2)
+				imagemColecao.definirImagemPadrao("50porcento");
+			else 
+				imagemColecao.definirImagemPadrao("20porcento");
+		}
 	}
 }
