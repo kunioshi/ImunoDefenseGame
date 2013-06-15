ed6db07d97f005e2b1543c819aa562fac3ab2460
Lucas <martino.mail@gmail.com>
2013-06-15 04:43:06 -0300

    fazendo o tiro ficar fluido

--- src/main/java/br/envyGames/imunoDefense/jogo/ia/TiroIA.java
+++ src/main/java/br/envyGames/imunoDefense/jogo/ia/TiroIA.java
@@ -5,11 +5,10 @@ import br.envyGames.imunoDefense.jogo.entidade.Tiro;
 import br.envyGames.imunoDefense.motor.Entidade;
 import br.envyGames.imunoDefense.motor.IAAcao;
 import br.envyGames.imunoDefense.motor.IAMensagem;
-import s3t.gameEntities.AIAction;
-import s3t.gameEntities.Entity;
-import s3t.gameEntities.IAMessage;
 
 public class TiroIA extends IAAcao {
+	private final int passo = 4;
+	
 	@Override
 	public void doAction(Entidade entity) {
 		if (entity instanceof Tiro)
@@ -22,24 +21,25 @@ public class TiroIA extends IAAcao {
 
 	@Override
 	public int timeToWait() {
-		return 300;
+		return 30;
 	}
 
 	private void andar(Tiro tiro) {
 		if(Tabuleiro.getTabuleiroAtual().converteCoordToGrid((int) tiro.getX()) < Tabuleiro.getTabuleiroAtual().converteCoordToGrid((int) tiro.getAlvo().getX())) {
-			tiro.doMove(32, 0);
+			tiro.doMove(passo, 0);
 		} else if(Tabuleiro.getTabuleiroAtual().converteCoordToGrid((int) tiro.getX()) > Tabuleiro.getTabuleiroAtual().converteCoordToGrid((int) tiro.getAlvo().getX())) {
-			tiro.doMove(-32, 0);
+			tiro.doMove(-1*passo, 0);
 		} else if(Tabuleiro.getTabuleiroAtual().converteCoordToGrid((int) tiro.getY()) < Tabuleiro.getTabuleiroAtual().converteCoordToGrid((int) tiro.getAlvo().getY())) {
-			tiro.doMove(0, 32);
+			tiro.doMove(0, passo);
 		} else if(Tabuleiro.getTabuleiroAtual().converteCoordToGrid((int) tiro.getY()) > Tabuleiro.getTabuleiroAtual().converteCoordToGrid((int) tiro.getAlvo().getY())) {
-			tiro.doMove(0, -32);
+			tiro.doMove(0, -1*passo);
 		}
 		
 		chegouProx(tiro);
 	}
 
 	private void chegouProx(Tiro tiro) {
+		
 		if( tiro.getCasaAtual().equals(Tabuleiro.getTabuleiroAtual().converteCoord((int)tiro.getAlvo().getX(), (int)tiro.getAlvo().getY())) ) {
 			darDano(tiro);
 			tiro.destruir();
